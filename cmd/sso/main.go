af1a4a18ea38533509b13b282d3eaf095e53c79a
Avichank <pashog123@yandex.ru>
2024-12-01 22:18:21 +0300

    feat: init config object

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,11 +1,40 @@
-package sso
+package main
+
+import (
+	"fmt"
+	"gRPC-auth/internal/config"
+	"log/slog"
+	"os"
+)
+
+	const (
+		envLocal = "local"
+		envDev = "dev"
+		envProd = "prod"
+	)
 
 func main() {
 	// инициализировать объект конфига
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
 
 	// инициализировать логгер
+
 
 	// инициализация приложения (app)
 
 	// запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,16 @@
 package config
 
-import "time"
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type Config struct {
 	Env         string        `yaml:"env" env-default:"local"`
-	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	StoragePath string        `yaml:"storage_path"`
 	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
 	GRPC        GRPCConfig    `yaml:"grpc"`
 }
@@ -15,9 +21,34 @@ type GRPCConfig struct {
 }
 
 func MustLoad() *Config {
+	path :=fetchConfigPath()
 
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exist: " + path)
+	}
+
+	 var cfg Config
+
+	 if err:=cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("failed to load config: " + err.Error())
+	 }
+
+	 return &cfg
 }
 
 func fetchConfigPath() string {
+	var res string
 	
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
 }
